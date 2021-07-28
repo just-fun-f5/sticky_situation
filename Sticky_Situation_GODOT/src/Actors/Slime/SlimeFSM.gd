e2608c512e4558ca31e60fa1e1c575d71e58924a
Shellowb <marcelo.becerra@ug.uchile.cl>
2021-07-28 00:13:03 -0400

    skills fixes
    Animations improvements to skill use, and absorb element change the animation

--- Sticky_Situation_GODOT/src/Actors/Slime/SlimeFSM.gd
+++ Sticky_Situation_GODOT/src/Actors/Slime/SlimeFSM.gd
@@ -1,5 +1,7 @@
 extends StateMachine
 
+var is_skill_used = false
+
 #Declare states
 func _ready():
 	add_state("idle")
@@ -7,14 +9,29 @@ func _ready():
 	add_state("jump")
 	add_state("fall")
 	add_state("wall_slide")
+	add_state("eat")
+	add_state("eating")
+	add_state("explode")
 	call_deferred("set_state", states.idle)
 
 #Jump input
 func _input(event):
+	if is_skill_used or state in [states.eat, states.eating]: return
+	
 	if !Game.is_net_master(self):
 		return
+	# SKILSS ACTIONS
 	if event.is_action_pressed("mb_right"):
-		parent._use_skill()
+		var used_skill = parent._use_skill()
+		if used_skill != null:
+			set_state(states[used_skill])
+			is_skill_used = true
+			
+	if event.is_action_pressed("skill_left"):
+		get_parent()._change_skill(1)
+	if event.is_action_pressed("skill_right"):
+		get_parent()._change_skill(-1)
+	# MOVEMENT ACTIONS
 	#Jump normally if run or idle state
 	if [states.idle, states.run].has(state):
 		if event.is_action_pressed("jump"):
@@ -30,13 +47,9 @@ func _input(event):
 	elif state == states.jump:
 		if event.is_action_released("jump") and parent.velocity.y < parent.min_jump_velocity:
 			parent.velocity.y = parent.min_jump_velocity
-	
-	if event.is_action_pressed("skill_left"):
-		get_parent()._change_skill(1)
-	if event.is_action_pressed("skill_right"):
-		get_parent()._change_skill(-1)
 
 func _state_logic(delta):
+	if is_skill_used: return
 	#We determine move direction
 	parent._update_move_direction()
 	#We determine wall direction based on move direction
@@ -54,6 +67,7 @@ func _state_logic(delta):
 
 #Transition conditions
 func _get_transition(delta):
+	if is_skill_used: return
 	match state:
 		#Idle state
 		states.idle:
@@ -103,21 +117,25 @@ func _get_transition(delta):
 
 func _enter_state(new_state, old_state):
 	#Apply animations when entering a new state:
+	var element = parent.current_element
+	var e_name = "" if (element == null or element.name.to_lower() == "slime") else ("_" + element.name.to_lower())
 	match new_state:
 		states.idle:
-			parent.anim_player.play("Idle")
+			parent.anim_player.play("Idle" + e_name)
 		states.run:
-			parent.anim_player.play("run")	
+			parent.anim_player.play("run" + e_name)
 		states.jump:
-			parent.anim_player.play("Jump")
+			parent.anim_player.play("Jump" + e_name)
 		states.fall:
-			parent.anim_player.play("Fall")
+			parent.anim_player.play("Fall" + e_name)
 		states.wall_slide:
-			parent.anim_player.play("wall_slide")
+			parent.anim_player.play("wall_slide" + e_name)
 			#Make sprite face away from wall
 			parent.body.scale.y = -parent.wall_direction
 			#Rotate sprite 
 			parent.body.set_rotation(1.5708)
+		states.eat:
+			parent.anim_player.play("Eat" + e_name)
 
 func _exit_state(old_state, new_state):
 	match old_state:
@@ -130,3 +148,10 @@ func _exit_state(old_state, new_state):
 func _on_WallSlideStickyTimer_timeout():
 	if state == states.wall_slide:
 		set_state(states.fall)
+
+
+func _on_AnimationPlayer2_animation_finished(anim_name):
+	if  state != states.eating and ("Eat" in anim_name or "explode" in anim_name):
+		is_skill_used = false
+		set_state(states.idle)
+	
